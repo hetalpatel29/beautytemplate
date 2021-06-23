03a17ec0546b6cf8f1404ed59fe8c86abe606833
hetalpatel29 <80544632+hetalpatel29@users.noreply.github.com>
2021-06-23 16:10:33 +0530

    my practice

---
+++ js/salontemplate.js
@@ -0,0 +1,39 @@
+// let a = document.getElementById('home');
+// console.log(a);
+
+// let b = document.getElementsByClassName('cosmeticbrands')[1];
+// console.log(b);
+
+// let c = document.getElementsByTagName('h3')[2];
+// console.log(c);
+
+// let d = document.querySelector('#blog');
+// console.log(d);
+
+// let e = document.querySelector('h4');
+// console.log(e);
+
+// let f = document.querySelectorAll('.customerreview')[0];
+// console.log(f);
+
+// // let g = document.querySelectorAll('#aboutsalon').style.Color = 'red';
+// // console.log(g);
+
+// let h = document.querySelector('#home').classList.add("red");
+// let i = document.querySelector('#togglemenu').classList.add("red");
+// let j = document.querySelector('#togglemenu').classList.remove("red");
+// let k = document.querySelector('#aboutsalon').classList.length;
+// console.log(k);
+
+// let l = document.querySelector('#aboutsalon').classList.item(0);
+// console.log(l);
+
+// let m = document.querySelector('#home').classList.contains('red');
+// console.log(m);
+// let n = document.querySelector('#home').classList.toggle('togglemenu');
+// console.log(n);
+
+function progiletoggle() {
+    document.querySelector('#profilebar').classList.toggle('menu');
+
+}
